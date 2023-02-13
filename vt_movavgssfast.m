2fb8b72d3c3ad44a4f2149e7d4827f567e5fd0d9
juliankeil <julian.keil@gmail.com>
2023-02-13 12:00:02 +0100

    stable version of moving average

--- vt_movavgssfast.m
+++ vt_movavgssfast.m
@@ -14,33 +14,79 @@ function [datamovavg,onsettimes]=vt_movavgssfast(cfg,varargin)
 %Nathan & Winny 2005
 %Nathan 2006: deleted ampcrit and added preallocation for speeding up
 % Julian 2023
-%% 0. set the cfgs
-modfreq = cfg.modfreq;
-cycnum = cfg.cycnum;
-discardcycles = cfg.discard;
-samprat = varargin{1}.fsample;
-wnd = cfg.window;
+%% 0. set the cfgs and defaults
+if isfield(cfg, 'modfreq')
+    modfreq = cfg.modfreq;
+else
+    fprintf('Please set the target frequency \n')
+    return
+end
 
-%% 1. Compute the ERP
+if isfield(cfg, 'cycnum')
+    cycnum = cfg.cycnum;
+else
+    cycnum = 5;
+    fprintf('No cycle number set, using the default of 5 cycles \n')
+end
+
+if isfield(cfg, 'discard')
+    discardcycles = cfg.discard;
+else
+    discardcycles = 1;
+    fprintf('No number of to-be-discarded cycles set, using the default of 1 cycle \n')
+end
+
+if isfield(cfg, 'window')
+    wnd = cfg.window;
+else
+    fprintf('Please set the analysis window \n')
+    return
+end
+
+if ~isfield(cfg, 'trials')
+    cfg.trials = 'all';
+    fprintf('Using all available trials \n')
+end
+
+if ~isfield(cfg, 'channel')
+    cfg.channel = 'all';
+    fprintf('Using all available channels \n')
+end
+
+if ~isfield(cfg, 'baseline')
+    cfg.baseline = [varargin{1}.time{1}(1) varargin{1}.time{1}(end)];
+    fprintf('Using the entire window for baseline correction \n')
+    return
+end
+
+%% 1. Downsample to maximum multiples of sample rate
+samprat = varargin{1}.fsample;
+icfg = [];
+icfg.resamplefs = modfreq*(floor(samprat/modfreq));
+
+tmp_dat = ft_resampledata(icfg,varargin{1});
+samprat = tmp_dat.fsample; % Update Sample rate
+
+%% 2. Compute the average across trials
 icfg = [];
 icfg.trials = cfg.trials;
 icfg.channel = cfg.channel;
 
-tmp_erp = ft_timelockanalysis(icfg,varargin{1});
+tmp_erp = ft_timelockanalysis(icfg,tmp_dat);
 
-% 1.1. Baseline correction
+% 2.1. Baseline correction
 icfg = [];
 icfg.baseline = cfg.baseline;
 
 tmp_erp = ft_timelockbaseline(icfg,tmp_erp);
 
-%% 2. compute the basic properties for the moving average
-x = samprat/1000; % how many points per ms
+%% 3. compute the basic properties for the moving average
+mspts = samprat/1000; % how many points per ms
 startms = discardcycles*1000/modfreq; %a when to start the moving average
 intervallms = 1000/modfreq; % one cycle in ms
-intervallspt = intervallms*x; % one cylce in sampling points
+intervallspt = intervallms*mspts; % one cylce in sampling points
 
-startspt = floor(startms*x);
+startspt = floor(startms*mspts);
 windowpoints = floor(cycnum*intervallspt);
 maxend = startspt+ceil(wnd*samprat)-windowpoints;
 
@@ -50,7 +96,6 @@ datamovavg.time = tmp_erp.time(startspt:startspt+windowpoints-1);
 datamovavg = rmfield(datamovavg,{'dof'});
 
 % 2.1 first go to preallocate ressources
-
 j=1;
 
 startms2 = startms;
@@ -58,12 +103,12 @@ startspt2 = startspt;
 
 while startspt2 < maxend
     startms2 = startms2+intervallms;
-    startspt2 = floor(startms2*x);
+    startspt2 = floor(startms2*mspts);
     j = j+1;
 end
 
-onsettimes = zeros(j,2);
-datatimecourse = zeros(size(tmp_erp.avg,1),windowpoints,j);
+onsettimes = NaN(j,2);
+datatimecourse = NaN(size(tmp_erp.avg,1),windowpoints,j);
 
 % 2.2 now real thing
 i=1;
@@ -74,10 +119,10 @@ while startspt < maxend
     datatimecourse(:,:,i) = tmp_erp.avg(:,startspt:startspt+windowpoints-1);
        
     startms = startms+intervallms;
-    startspt = floor(startms*x);
+    startspt = floor(startms*mspts);
     i = 1+i;
 end
 
 % Collect the output
-datamovavg.avg = mean(datatimecourse,3);
+datamovavg.avg = mean(datatimecourse,3,'omitnan');
 
